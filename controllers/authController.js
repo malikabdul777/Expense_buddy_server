f60f74aee7ce40c6ac8ca9c3e380d6518aa0ef99
Abdul Malik Shaik <malik7771@yahoo.com>
2024-04-10 18:03:11 -0400

    Add signin controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
     const { name, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -21,6 +22,48 @@ exports.signup = async (req, res) => {
         user: newUser,
       },
     });
+  } catch (err) {
+    res.status(400).json({
+      status: "Request Failed",
+      message: err,
+    });
+  }
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return res.status(404).json({
+        status: "Request Failed",
+        message: "User not found",
+      });
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(401).json({
+        status: "Request Failed",
+        message: "Wrong login credentials",
+      });
+    }
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day expiry
+      })
+      .status(200)
+      .json({
+        status: "success",
+        user: rest,
+      });
   } catch (err) {
     res.status(400).json({
       status: "Request Failed",
